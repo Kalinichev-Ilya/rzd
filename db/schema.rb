f6584095ea043f9d1dc4c8b08159dcd1cf734c8c
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-10-11 11:47:20 +0300

    clean models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171004201026) do
+ActiveRecord::Schema.define(version: 20171005210914) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -20,10 +20,24 @@ ActiveRecord::Schema.define(version: 20171004201026) do
 
   create_table "routes", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "trains", force: :cascade do |t|
     t.string "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
