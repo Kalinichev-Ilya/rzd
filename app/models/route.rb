59c3c4dcbf59f17dbfda739c78db47f83cd1a486
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-10-13 22:50:35 +0300

    changed sequence of declaring validations and associations

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,12 +1,11 @@
 class Route < ApplicationRecord
+  has_many :railway_stations_routes
+  has_many :railway_stations, through: :railway_stations_routes
+  has_many :trains
+
   validates :name, presence: true,
                    format: { with: /\A[a-z]+-? ?[a-z]+\z/i,
                              message: "Name doesn't match with format." },
                    length: { in: 6..20 },
                    uniqueness: true
-
-  has_many :railway_stations_routes
-  has_many :railway_stations, through: :railway_stations_routes
-
-  has_many :trains
 end
