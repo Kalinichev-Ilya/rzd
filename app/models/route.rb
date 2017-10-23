76a20cf335bb8e87022064fcf8dc0936194ff810
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-10-24 00:41:49 +0300

    optimize regexp

--- app/models/route.rb
+++ app/models/route.rb
@@ -4,7 +4,7 @@ class Route < ApplicationRecord
   has_many :trains
 
   validates :name, presence: true,
-                   format: { with: /\A[a-z-]+-? ?[a-z-]+\z/i,
+                   format: { with: /\A[a-z-]+([- ])?[a-z-]+\z/i,
                              message: "Name doesn't match with format." },
                    length: { in: 6..30 },
                    uniqueness: true
