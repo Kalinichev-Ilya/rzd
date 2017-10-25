199577d1b13c7f06b78235579fc2063c35879d3c
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-10-25 03:08:18 +0300

    added wagons count by grade and seats counts by type methods

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -1,6 +1,8 @@
 class TrainsController < ApplicationController
   before_action :set_train, only: %i[show edit update destroy]
   before_action :set_routes, only: %i[new create edit update]
+  before_action :wagons_count_by_type, only: %i[show]
+  before_action :seats_count_by_wagon_type, only: %i[show]
 
   def index
     @trains = Train.all
@@ -49,5 +51,30 @@ class TrainsController < ApplicationController
 
   def train_params
     params.require(:train).permit(:number, :route_id)
+  end
+
+  # {
+  #     berth: 3,
+  #     roomette: 5
+  # }
+  def wagons_count_by_type
+    @wagons_count = {}
+    @wagons_count['berth'] = @train.wagons.where(grade: 'berth').count
+    @wagons_count['roomette'] = @train.wagons.where(grade: 'roomette').count
+  end
+
+  # {
+  #     berth: { upper: 10, bottom: 15 },
+  #     roomette: { upper: 10, bottom: 15 }
+  # }
+  def seats_count_by_wagon_type
+    @seats_count = {}
+    berth_upper = Train.last.wagons.where(grade: 'berth').sum(:upper_seats_count)
+    berth_bottom = Train.last.wagons.where(grade: 'berth').sum(:bottom_seats_count)
+    roomette_upper = Train.last.wagons.where(grade: 'roomette').sum(:upper_seats_count)
+    roomette_bottom = Train.last.wagons.where(grade: 'roomette').sum(:bottom_seats_count)
+
+    @seats_count['berth'] = { upper: berth_upper, bottom: berth_bottom }
+    @seats_count['roomette'] = { upper: roomette_upper, bottom: roomette_bottom }
   end
 end
