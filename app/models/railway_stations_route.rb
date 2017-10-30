96775c6e5636a1ebb03cefb4909e174a5d3c799f
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-10-31 00:14:51 +0300

    uniqueness station_number in route

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -1,4 +1,7 @@
 class RailwayStationsRoute < ApplicationRecord
   belongs_to :railway_station
   belongs_to :route
+
+  validates :station_number, uniqueness: { scope: %i[railway_station_id route_id],
+                                           message: 'scope message' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171310210914) do
+ActiveRecord::Schema.define(version: 20171410210914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171310210914) do
   create_table "railway_stations_routes", force: :cascade do |t|
     t.bigint "railway_station_id"
     t.bigint "route_id"
+    t.integer "station_number"
     t.index ["railway_station_id"], name: "index_railway_stations_routes_on_railway_station_id"
     t.index ["route_id"], name: "index_railway_stations_routes_on_route_id"
   end
