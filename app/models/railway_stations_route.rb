d306d3b849df8257e3f86baa6f5699708c4884ad
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-11-06 02:34:13 +0300

    order railway stations in route by station number

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -1,7 +1,8 @@
 class RailwayStationsRoute < ApplicationRecord
   belongs_to :railway_station
   belongs_to :route
-
+  
   validates :station_number, uniqueness: { scope: %i[route_id],
-                                           message: 'scope message' }
+                                           message: 'Railway station already exists'}
+  default_scope { order(:station_number) }
 end
