664d360c3ee8ef72b49dea50dced8493e3c8a144
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-11-05 23:31:50 +0300

    station_number uniq by route_id

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -2,6 +2,6 @@ class RailwayStationsRoute < ApplicationRecord
   belongs_to :railway_station
   belongs_to :route
 
-  validates :station_number, uniqueness: { scope: %i[railway_station_id route_id],
+  validates :station_number, uniqueness: { scope: %i[route_id],
                                            message: 'scope message' }
 end
