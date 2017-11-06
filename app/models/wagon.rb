2abcfd178403019df0547513eb301095c0527bfc
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-11-06 23:20:57 +0300

    create number before validation, number uniq

--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -1,9 +1,15 @@
 class Wagon < ApplicationRecord
   belongs_to :train
 
-  validates :number, presence: true
+  # added for complete the lesson, it's not needed
+  validates :number, presence: true, uniqueness: { scope: %i[train_id],
+                                                   message: 'This number already exists'}
   validates :grade, inclusion: { in: %w[berth roomette],
                                  message: 'Invalid Type: wagon can be only roomette or berth.' }
+
+  before_validation do
+    self.number = train.wagons.count + 1
+  end
 
   enum grade: {
     berth: 'berth',
