796c06019e68c88e38edd8f2f8785e5668eae856
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-11-06 23:04:06 +0300

    create station number by operation

--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -1,6 +1,7 @@
 class Wagon < ApplicationRecord
   belongs_to :train
 
+  validates :number, presence: true
   validates :grade, inclusion: { in: %w[berth roomette],
                                  message: 'Invalid Type: wagon can be only roomette or berth.' }
 
--- app/operations/create_point.rb
+++ app/operations/create_point.rb
@@ -1,11 +1,9 @@
-module Operations
-  module CreatePoint
-    extend self
-
-    def call(route, station, number)
-      RailwayStationsRoute.create(route: route,
-                                  railway_station: station,
-                                  station_number: number)
-    end
+module CreatePoint
+  extend self
+  
+  def call(route, station, number)
+    RailwayStationsRoute.create(route: route,
+                                railway_station: station,
+                                station_number: number)
   end
 end
