4c6d4dae9feff65fcb684d9c35c2656caf840f32
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-11-09 00:13:09 +0300

    try coffee-script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require rails-ujs
+//= require jquery3
+//= require jquery-ujs
 //= require popper
 //= require turbolinks
 //= require_tree .
