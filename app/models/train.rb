86bd5091bd9c19fd7660a1e3fbefe5cee2f70a40
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-11-09 00:17:08 +0300

    remove comments

--- app/models/train.rb
+++ app/models/train.rb
@@ -12,16 +12,4 @@ class Train < ApplicationRecord
   def count(wagon_type, seats_type)
     wagons.where(type: wagon_type).sum(seats_type)
   end
-  
-  # def seats_count_by_wagon_type
-  #   berth_upper = wagons.berth.sum(:top_seats)
-  #   berth_bottom = wagons.berth.sum(:bottom_seats)
-  #   roomette_upper = wagons.roomette.sum(:top_seats)
-  #   roomette_bottom = wagons.roomette.sum(:bottom_seats)
-  #
-  #   {
-  #     berth: { upper: berth_upper, bottom: berth_bottom },
-  #     roomette: { upper: roomette_upper, bottom: roomette_bottom }
-  #   }
-  # end
 end
