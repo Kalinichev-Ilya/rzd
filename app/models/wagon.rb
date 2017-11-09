7722038e4adc5d55c2594863cb22d53a6b8101ba
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-11-10 00:33:02 +0300

    fix bug with :number

--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -5,21 +5,22 @@ class Wagon < ApplicationRecord
   validates :number, presence: true, uniqueness: { scope: %i[train_id],
                                                    message: 'This number already exists' }
 
-  before_validation do
-    self.number = train.wagons.pluck(:number).max + 1
+  before_validation :set_number, on: :create
+
+  def seats_info
+    { side_top_seats: side_top_seats,
+      side_bottom_seats: side_bottom_seats,
+      top_seats: top_seats,
+      bottom_seats: bottom_seats }
   end
 
-  def show_seats_count
-    case type
-    when 'SvWagon'
-      { bottom_seats: bottom_seats }
-    when 'CoupeWagon'
-      { bottom_seats: bottom_seats, top_seats: top_seats }
-    when 'EconomyWagon'
-      { side_top_seats: side_top_seats, side_bottom_seats: side_bottom_seats,
-        top_seats: top_seats, bottom_seats: bottom_seats }
-    when 'SeatingWagon'
-      { seating_seats: seating_seats }
-    end
+  private
+
+  def set_number
+    self.number = if train.wagons.any?
+                    train.wagons.maximum(:number) + 1
+                  else
+                    1
+                  end
   end
 end
