98be7d8e47e7ce53497a2319ff477f509cf1d534
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-11-25 20:50:03 +0300

    show form upgraded for set_station number

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
 
-  resources :railway_stations
+  resources :railway_stations do
+    patch :set_position, on: :member
+  end
   resources :trains
   resources :routes
   resources :wagons
