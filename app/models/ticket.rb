4f715ad878a47ca2b442ea7a7ea88f41b3d977d8
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-12-11 04:36:36 +0300

    add callback for number generation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,5 +4,18 @@ class Ticket < ApplicationRecord
   belongs_to :to_station, class_name: 'RailwayStation'
   belongs_to :user, optional: true
 
-  validates :number, presence: true
+  before_validation :generate_number
+
+  validates :number, presence: true, uniqueness: true
+
+  private
+
+  def generate_number
+    nums = []
+    chars = []
+    3.times { nums << (0..9).to_a.sample }
+    2.times { chars << ('A'..'Z').to_a.sample }
+
+    self.number = "#{nums.join}-#{chars.join}"
+  end
 end
