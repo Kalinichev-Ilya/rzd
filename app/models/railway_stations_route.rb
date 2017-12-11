9570744409092a3ccff7c531d2c2ad67474a572d
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-12-12 00:58:46 +0300

    ..

--- app/models/railway_stations_route.rb
+++ app/models/railway_stations_route.rb
@@ -2,6 +2,8 @@ class RailwayStationsRoute < ApplicationRecord
   belongs_to :railway_station
   belongs_to :route
 
+  before_validation
+
   validates :station_number, uniqueness: { scope: :route_id,
                                            message: 'Railway station already exists' }
   default_scope { order(:station_number) }
