b5daf235c5ddf1ae256658aa16b9f499bc569e87
Ilya Kalinichev <iluya.kalinichev@gmail.com>
2017-12-11 04:37:42 +0300

    add passport, registration logic

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,25 @@
 class User < ApplicationRecord
   has_many :tickets
+  has_one :passport
 
-  validates :name, presence: true
+  def self.registration(passport)
+    user = User.create!(name: passport['name'])
+    Passport.create!(user: user,
+                     idd: passport[:idd],
+                     series: passport[:series],
+                     issue_date: passport[:issue_date],
+                     issuer: passport[:issuer],
+                     division_code: passport[:division_code],
+                     name: passport[:name],
+                     surname: passport[:surname],
+                     birth_date: passport[:birth_date])
+    user
+  end
+
+  def buy(ticket)
+    tickets << ticket
+  end
+
+  # validates :name, presence: true
+
 end
